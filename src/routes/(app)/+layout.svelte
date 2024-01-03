8099d4bafd25afe7d85ad6179e8d2a6a82283962
Matiss <matiss.aprups@fleetcomplete.com>
2024-01-03 14:29:40 +0200

    base layout setup

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import '../../app.pcss';
+	import { ModeWatcher } from 'mode-watcher';
+	import { Button } from '$lib/components/ui/button';
+	import { Moon, Sun } from 'lucide-svelte';
+	import { toggleMode } from 'mode-watcher';
+</script>
+
+<ModeWatcher />
+
+<div class="flex min-h-screen flex-col pt-16">
+
+	<header class="fixed background foreground top-0 w-full border-b p-4 shadow-sm">
+		<nav class="container mx-auto flex items-center justify-between">
+			<!-- Navbar Content -->
+			<div>
+				<a href="/" class="text-xl font-semibold">shadcn-svelte </a>
+			</div>
+
+			<div class="hidden items-center space-x-4 md:flex">
+				<a href="/login">
+					<Button variant="ghost">
+						Sign in
+					</Button>
+				</a>
+				
+				<Button on:click={toggleMode} size="icon" variant="ghost">
+					<Moon size={20}
+						class="rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+					/>
+					<Sun size={20}
+						class="absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+					/>
+				</Button>
+			</div>
+		</nav>
+	</header>
+
+	<div class="container mx-auto flex-grow p-4">
+		<slot />
+	</div>
+
+	<footer class="p-4 mt-auto border-t shadow-md background foreground">
+		<div class="container mx-auto">
+			<!-- Footer content goes here -->
+			<p>&copy; 2024. All rights reserved.</p>
+		</div>
+	</footer>
+</div>
---
+++ src/routes/(login)/login/+page.svelte
@@ -0,0 +1,111 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	import { Label } from '$lib/components/ui/label';
+	import { cn } from '$lib/utils';
+	import { Loader2 } from 'lucide-svelte';
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+
+	let isLoading = false;
+	async function onSubmit() {
+		isLoading = true;
+
+		setTimeout(() => {
+			isLoading = false;
+		}, 3000);
+	}
+</script>
+
+<div
+	class="container relative min-h-screen flex-col items-center justify-center grid lg:max-w-none lg:grid-cols-2 lg:px-0"
+>
+	<!-- <Button
+		href="/"
+		variant="ghost"
+		class="absolute right-4 top-4 md:right-8 md:top-8"
+	>
+		Home
+	</Button> -->
+	<div class="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
+		<div
+			class="absolute inset-0 bg-cover"
+			style="
+				background-image:
+					url(https://images.unsplash.com/photo-1590069261209-f8e9b8642343?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1376&q=80);"
+		/>
+		<div class="relative z-20 flex items-center text-lg font-medium">
+			<a href="/">shadcn-svelte</a>
+		</div>
+		<div class="relative z-20 mt-auto">
+			<blockquote class="space-y-2">
+				<p class="text-lg">
+					&ldquo;This library has saved me countless hours of work and helped me deliver stunning
+					designs to my clients faster than ever before. Highly recommended!&rdquo;
+				</p>
+				<footer class="text-sm">Sofia Davis</footer>
+			</blockquote>
+		</div>
+	</div>
+	<div class="lg:p-8">
+		<div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+			<div class="flex flex-col space-y-2 text-center">
+				<h1 class="text-2xl font-semibold tracking-tight">Create an account</h1>
+				<p class="text-sm text-muted-foreground">Enter your email below to create your account</p>
+			</div>
+			<div class={cn('grid gap-6', className)} {...$$restProps}>
+				<form on:submit|preventDefault={onSubmit}>
+					<div class="grid gap-2">
+						<div class="grid gap-1">
+							<Label class="sr-only" for="email">Email</Label>
+							<Input
+								id="email"
+								placeholder="name@example.com"
+								type="email"
+								autocapitalize="none"
+								autocomplete="email"
+								autocorrect="off"
+								disabled={isLoading}
+							/>
+						</div>
+						<Button disabled={isLoading}>
+							{#if isLoading}
+								<Loader2 class="mr-2 h-4 w-4 animate-spin" />
+							{/if}
+							Sign In with Email
+						</Button>
+					</div>
+				</form>
+				<div class="relative">
+					<div class="absolute inset-0 flex items-center">
+						<span class="w-full border-t" />
+					</div>
+					<div class="relative flex justify-center text-xs uppercase">
+						<span class="bg-background px-2 text-muted-foreground"> Or continue with </span>
+					</div>
+				</div>
+				<Button variant="outline" type="button" disabled={isLoading}>
+					{#if isLoading}
+						<!-- <Icons.spinner class="mr-2 h-4 w-4 animate-spin" /> -->
+					{:else}
+						<!-- <Icons.gitHub class="mr-2 h-4 w-4" /> -->
+					{/if}
+					{' '}
+					GitHub
+				</Button>
+			</div>
+			<p class="px-8 text-center text-sm text-muted-foreground">
+				By clicking continue, you agree to our{' '}
+				<a href="/terms" class="underline underline-offset-4 hover:text-primary">
+					Terms of Service
+				</a>{' '}
+				and{' '}
+				<a href="/privacy" class="underline underline-offset-4 hover:text-primary">
+					Privacy Policy
+				</a>
+				.
+			</p>
+		</div>
+	</div>
+</div>
